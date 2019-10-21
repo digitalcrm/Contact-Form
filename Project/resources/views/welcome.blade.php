cadcd7b7a16952097c2629e0e413fb6f9aacfa7a
digitalcrm <info@digitalcrm.com>
2019-10-21 19:25:02 +0530

    Upadated in L6

--- Project/resources/views/welcome.blade.php
+++ Project/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Contact Form
                 </div>
 
                 <div class="links">
